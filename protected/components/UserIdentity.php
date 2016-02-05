4ef934a79c048ed342891e023321914f393a48a6
pilli manikanta swamy <swamy81293@gmail.com>
2016-02-05 14:15:19 +0530

    change the authentication to the database

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -17,17 +17,15 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
+		$record=User::model()->findByAttributes(array('user_email'=>$this->username));
+		if($record===null)
+				$this->errorCode=self::ERROR_USERNAME_INVALID;
+		elseif ($record->user_password!==$this->password)
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
+		else 
+		{
 			$this->errorCode=self::ERROR_NONE;
+		}
 		return !$this->errorCode;
 	}
 }
